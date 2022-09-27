b081808958d4721ebc656a36aaeeb0dbf45b0962
Long Hoang <long@mindworker.de>
2022-09-27 11:27:13 +0800

    Add benchmark results as comments

--- print_test.go
+++ print_test.go
@@ -6,6 +6,21 @@ import (
 	"testing"
 )
 
+// fmt.Println("Hello " + worldString)
+// vs
+// fmt.Println("Hello", worldString)
+//
+// Result:
+// =======
+// goos: darwin
+// goarch: amd64
+// pkg: github.com/loong/go-surprising-benchmarks
+// cpu: Intel(R) Core(TM) i5-1038NG7 CPU @ 2.00GHz
+// BenchmarkConcat-8       95025295               111.5 ns/op
+// BenchmarkNativeFmt-8    151688880               79.84 ns/op
+// PASS
+// ok      github.com/loong/go-surprising-benchmarks       31.166s
+
 func BenchmarkConcat(b *testing.B) {
 	temp := os.Stdout
 	os.Stdout = nil    // turn it off
